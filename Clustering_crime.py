971b16db96c5ec692af82a06da8d879336451e4b
SharanKumar1296 <123482780+SharanKumar1296@users.noreply.github.com>
2023-08-16 16:56:09 +0530

    Add files via upload

---
+++ Clustering_crime.py
@@ -0,0 +1,129 @@
+#===================================================================================
+""" Using the different ways of Cluster Analysis on the crime dataset and drawing 
+inferences. """
+
+import pandas as pd 
+
+df = pd.read_csv("crime_data.csv")
+
+df.head()
+df.shape
+df.dtypes
+
+from sklearn.preprocessing import MinMaxScaler
+mm = MinMaxScaler()
+
+df.iloc[:,1:] = mm.fit_transform(df.iloc[:,1:])
+
+X = df.iloc[:,1:]
+
+#Using K-Means Clustering
+#Building an elbow plot
+from sklearn.cluster import KMeans
+clust = []
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters=i,random_state=0)
+    kmeans.fit(X)
+    clust.append(kmeans.inertia_)
+
+import matplotlib.pyplot as plt 
+plt.scatter(x=range(1, 11), y=clust,color='red')
+plt.plot(range(1, 11), clust,color='black')
+plt.title('Elbow Method')
+plt.xlabel('Number of clusters')
+plt.ylabel('inertial values')
+plt.show()
+#We can see using the elbow curve that the best number of clusters is 4.
+
+kmeans = KMeans(n_clusters=4,n_init=20)
+
+# Fitting with inputs
+kmeans = kmeans.fit(X)
+# Predicting the clusters
+Y = kmeans.predict(X)
+Y_new = pd.DataFrame(Y)
+Y_new[0].value_counts() #2    17
+                        #0    13
+                        #3    12
+                        #1     8
+
+#We can observe that using Kmeans clustering we are able to form clusters in 
+#accordance to the elbow plot information 
+
+#Using Agglomerative Clustering
+import scipy.cluster.hierarchy as shc 
+plt.figure(figsize=(10,7))
+plt.title("Dendograms")
+dend = shc.dendrogram(shc.linkage(X,method="single"))
+
+from sklearn.cluster import AgglomerativeClustering
+cluster = AgglomerativeClustering(n_clusters=4,affinity="euclidean",linkage="single")
+Y = cluster.fit_predict(X)
+
+Y_new = pd.DataFrame(Y)
+Y_new.value_counts()  # 0    47
+                      # 1     1
+                      # 2     1
+                      # 3     1
+
+plt.figure(figsize=(10,7))
+plt.title("Dendograms")
+dend = shc.dendrogram(shc.linkage(X,method="complete"))
+
+from sklearn.cluster import AgglomerativeClustering
+cluster = AgglomerativeClustering(n_clusters=4,affinity="euclidean",linkage="complete")
+Y = cluster.fit_predict(X)
+
+Y_new = pd.DataFrame(Y)
+Y_new.value_counts() # 1    20
+                     # 3    12
+                     # 2    10
+                     # 0     8
+
+
+plt.figure(figsize=(10,7))
+plt.title("Dendograms")
+dend = shc.dendrogram(shc.linkage(X,method="average"))
+
+from sklearn.cluster import AgglomerativeClustering
+cluster = AgglomerativeClustering(n_clusters=4,affinity="euclidean",linkage="average")
+Y = cluster.fit_predict(X)
+
+Y_new = pd.DataFrame(Y)
+Y_new.value_counts() # 0    30
+                     # 1    12
+                     # 3     7
+                     # 2     1
+
+#Above we have used the multiple methods of agglomerative clustering and have come up
+#with 4 clusters for each.
+
+#Using DBSCAN to create clusters
+from sklearn.cluster import DBSCAN
+db = DBSCAN(eps=0.60,min_samples=2)  
+
+db.fit(X)
+
+Y = db.labels_ #The labels created after the clustering 
+pd.DataFrame(Y).value_counts() # 0   50
+
+db = DBSCAN(eps=0.25,min_samples=3)  
+
+db.fit(X)
+
+Y = db.labels_ #The labels created after the clustering 
+pd.DataFrame(Y).value_counts() 
+
+#Tried and tested multiple epsilon values and also multiple samples. We can see
+#the formation of clusters in the second split.
+
+df["cluster"] = pd.DataFrame(Y)
+
+noise_points = df[df["cluster"]==-1] #Separating the noise points from the data 
+
+final_data = df[df["cluster"]!=-1] #Creating a new dataframe without outliers
+
+#We have tried and tested multiple clustering techniques and come up with clusters for 
+#the given dataset.
+
+#===================================================================================
