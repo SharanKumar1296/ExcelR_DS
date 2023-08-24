6e746da217238c09111fd1e2df6e67f857c6698e
SharanKumar1296 <123482780+SharanKumar1296@users.noreply.github.com>
2023-08-24 15:36:09 +0530

    Update SVM_forest_fires.py

--- SVM_forest_fires.py
+++ SVM_forest_fires.py
@@ -73,4 +73,8 @@ from sklearn.metrics import accuracy_score
 print("Training accuracy is",(accuracy_score(Y_train,Y_train_pred)*100).round(2)) #74.42
 print("Testing accuracy is",(accuracy_score(Y_test,Y_test_pred)*100).round(2)) #70.77
 
+
+#We have used different kernels and multiple parameters for those kernels and have developed
+#the best accuracy scores in accordance with the kernels.
+
 #==============================================================================
